2aaea56e9e7ff08a322c7ac5d16b83787f233251
Serhii Veretennikov <gridis72@gmail.com>
2024-07-30 17:05:48 +0200

    feat: added strcmp and fixed some norm errors

--- src/libft/ft_strcmp.c
+++ src/libft/ft_strcmp.c
@@ -1,29 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/18 12:31:16 by svereten          #+#    #+#             */
-/*   Updated: 2024/07/30 17:05:19 by svereten         ###   ########.fr       */
+/*   Created: 2024/07/30 17:03:15 by svereten          #+#    #+#             */
+/*   Updated: 2024/07/30 17:04:35 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "libft/libft.h"
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
-
-# ifndef MAX_FD
-#  define MAX_FD 1024
-# endif
-
-# include "libft/libft.h"
-# include <stdlib.h>
-# include <unistd.h>
-
-char	*get_next_line(int fd);
-
-#endif
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	return (ft_strncmp(s1, s2, ft_strlen(s1)));
+}
