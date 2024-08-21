341022118593a651683730db88526b98d1c773e9
Serhii Veretennikov <gridis72@gmail.com>
2024-08-21 16:54:52 +0200

    fix: fixed potential strarrdup leak

--- src/libft/ft_strarrdup.c
+++ src/libft/ft_strarrdup.c
@@ -6,7 +6,7 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 17:20:19 by svereten          #+#    #+#             */
-/*   Updated: 2024/08/18 15:06:58 by svereten         ###   ########.fr       */
+/*   Updated: 2024/08/21 16:54:20 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft/libft.h"
@@ -23,16 +23,13 @@ char	**ft_strarrdup(char **arr)
 
 	res = (char **)ft_calloc(ft_strarrlen(arr) + 1, sizeof(char *));
 	if (!arr || !res)
-		return (NULL);
+		return (free(res), NULL);
 	i = 0;
 	while (arr[i])
 	{
 		res[i] = ft_strdup(arr[i]);
 		if (!res[i])
-		{
-			ft_free(STR_ARR, res);
-			return (NULL);
-		}
+			return (ft_free(STR_ARR, res), NULL);
 		i++;
 	}
 	return (res);
