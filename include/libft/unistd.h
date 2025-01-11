24f9b313e5a606f25369d19854229cbf4d805af3
Serhii Veretennikov <gridis72@gmail.com>
2025-01-11 17:21:56 +0100

    feat: removed in library panics

--- include/libft/unistd.h
+++ include/libft/unistd.h
@@ -6,13 +6,13 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 17:35:51 by svereten          #+#    #+#             */
-/*   Updated: 2024/11/26 17:36:26 by svereten         ###   ########.fr       */
+/*   Updated: 2025/01/11 17:21:18 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef UNISTD_H
 # define UNISTD_H
 
-void	ft_close(int fd);
-int		ft_open(char *path, int oflag, int perm);
+int	ft_close(int fd);
+int	ft_open(char *path, int oflag, int perm);
 
 #endif
--- src/unistd/ft_close.c
+++ src/unistd/ft_close.c
@@ -6,21 +6,21 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 16:58:40 by svereten          #+#    #+#             */
-/*   Updated: 2024/11/26 17:36:47 by svereten         ###   ########.fr       */
+/*   Updated: 2025/01/11 17:20:13 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "gc.h"
-#include "libft/stdlib.h"
 #include <unistd.h>
 
-void	ft_close(int fd)
+int	ft_close(int fd)
 {
 	t_data	data;
 
 	if (fd < 0)
-		return ;
+		return (0);
 	if (close(fd) == -1)
-		ft_panic(1, NULL);
+		return (-1);
 	data.fd = fd;
 	gc_data_remove(FD, data);
+	return (0);
 }
