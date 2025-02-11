80763f332637adb2542c659aa490ac2ada8575d5
Serhii Veretennikov <gridis72@gmail.com>
2025-02-11 13:32:08 +0100

    fix: strcmp now checks strings including null terminator of s1

--- src/string/ft_strcmp.c
+++ src/string/ft_strcmp.c
@@ -6,12 +6,12 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 17:03:15 by svereten          #+#    #+#             */
-/*   Updated: 2024/11/26 17:16:27 by svereten         ###   ########.fr       */
+/*   Updated: 2025/02/11 13:30:42 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft/string.h"
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	return (ft_strncmp(s1, s2, ft_strlen(s1)));
+	return (ft_strncmp(s1, s2, ft_strlen(s1) + 1));
 }
