fffd64847812815625fbcba80ed6ad091ba02308
GridisBlueFeathers <gridis72@gmail.com>
2025-02-21 17:35:45 +0100

    fix: added ignoring padding zeroes for ft_isnumber

--- src/ctype/ft_isnumber.c
+++ src/ctype/ft_isnumber.c
@@ -6,18 +6,28 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:38:07 by svereten          #+#    #+#             */
-/*   Updated: 2025/02/21 17:13:10 by svereten         ###   ########.fr       */
+/*   Updated: 2025/02/21 17:33:40 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft/ctype.h"
 #include "libft/string.h"
 
+static int	find_beginning(char *str)
+{
+	int	res;
+
+	res = 0;
+	if (str[0] == '-' || str[0] == '+')
+		res++;
+	while (str[res] && str[res] == '0')
+		res++;
+	return (res);
+}
+
 static int	basic_check(char *str)
 {
 	int	i;
 
-	if (ft_strlen(str) > 20)
-		return (0);
 	i = 0;
 	if (str[0] == '-' || str[0] == '+')
 		i++;
@@ -36,15 +46,18 @@ static int	basic_check(char *str)
  */
 static int	overflow_check(char *str)
 {
-	if ((str[0] == '-' || str[0] == '+') && ft_strlen(str) < 20)
+	int	i;
+
+	i = find_beginning(str);
+	if (ft_strlen(&str[i]) < 19)
 		return (1);
-	if (ft_strlen(str) < 19)
+	if (ft_strlen(&str[i]) > 19)
+		return (0);
+	if (str[0] == '-' && ft_strcmp(&str[i], "9223372036854775808") <= 0)
 		return (1);
-	if (str[0] == '-' && ft_strcmp(&str[1], "9223372036854775808") <= 0)
+	if (str[0] == '+' && ft_strcmp(&str[i], "9223372036854775807") <= 0)
 		return (1);
-	if (str[0] == '+' && ft_strcmp(&str[1], "9223372036854775807") <= 0)
-		return (1);
-	if (ft_isdigit(str[0]) && ft_strcmp(str, "9223372036854775807") <= 0)
+	if (ft_isdigit(str[0]) && ft_strcmp(&str[i], "9223372036854775807") <= 0)
 		return (1);
 	return (0);
 }
